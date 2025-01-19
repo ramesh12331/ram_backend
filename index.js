503ae73ccf79137cba564b5115a07468326d786f
ramesh12331 <mamidiramesh503@gmail.com>
2025-01-20 01:04:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const swaggerJsdoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
 
 require("dotenv").config();
-var cors = require("cors");
+const cors = require("cors");
 
 const app = express();
 
